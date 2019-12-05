465f7f0d31b6d1b9f580430b69514c3811f84fd5
Jusan Magno <jusanmagno@gmail.com>
2019-12-05 16:47:08 +0000

    inicio de projeto

--- app.js
+++ app.js
@@ -1,8 +1,18 @@
 const Express = require("express")
 const app = new Express()
 
+var Calculo = require("./modulos/calculo")
+var calculo = new Calculo();
+
+
 app.get("/", function(req, res) {
     res.sendFile(__dirname + "/views/index.html")
+})
+
+app.post("/save", function (req, res) {
+    var kmInicial = parseInt(res.body.kmInicial)
+    var kmFinal = parseInt(res.body.kmFinal)
+    var consumo = calculo()
 })
 
 listen(5000, "fuelcontroller", function() {
